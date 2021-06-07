d59660d180e22159d4f376ba8549399e2990b090
MeechaPooch <powchm@catlin.edu>
2021-06-06 21:27:20 -0700

    move from badwords to badwordsNoComp

--- test.js
+++ test.js
@@ -1,4 +1,7 @@
-// import {Filter} from './profanity-filter.js'
+import {Filter} from './profanity-filter.js'
 
-// let c = new Filter()
-// c.loadDefault()
+let c = new Filter()
+c.loadDefault()
+
+console.log(c.isVulgar('cing '))
+console.log(c.compressor.compress('cing '))
